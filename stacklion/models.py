a87129b4ea048eda2fdb5dc32fcc4881be0f53d9
Chloekkk <51353146+Chloekkk@users.noreply.github.com>
2020-05-09 19:47:46 +0900

    update qna file uploading&downloading

--- stacklion/models.py
+++ stacklion/models.py
@@ -3,10 +3,14 @@ from django.db import models
 # Create your models here.
 from account.models import User
 
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
 
 class Question(models.Model):
     #pk 자동생성
     question_content = models.TextField(null=False, blank=False)
+    question_file = models.FileField(upload_to='files/', null=True, blank=True)
     pub_date = models.DateField('date published')
     asker = models.ForeignKey(User, on_delete=models.PROTECT)
     def __str__(self):
--- stacklion/urls.py
+++ stacklion/urls.py
@@ -1,7 +1,10 @@
+from django.conf.urls.static import static
 from django.urls import path
+
+from config import settings
 from . import views
 
 urlpatterns = [
     path('', views.QnA, name = 'QnA'),
     path('answer/', views.refresh, name='refresh')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
